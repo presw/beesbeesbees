efe5903e63a0519914654b143abf6a7d44773bf9
Preston Whitehall <pmwhitehall@gmail.com>
2019-07-22 21:28:22 -0700

    Added RetiredForagerBee class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  var string = 'I am too old, let me play cards instead';
+  return string;
+};
+
+RetiredForagerBee.prototype.gamble = ForagerBee.prototype.forage;
